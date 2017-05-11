21d18af6b4199d6fafab5a21e1064bd2c1dc7497
SoldierGamma <michael@tangentsolutions.co.za>
2017-05-11 12:27:16 +0200

    Models implemented without functions

--- api/docservice/models.py
+++ api/docservice/models.py
@@ -5,4 +5,15 @@ from django.db import models
 # Create your models here.
 class Document(models.Model):
     name = models.CharField(max_length=50)
-    image = models.ImageField()
+    image = models.FileField()
+
+class ImageDocument(models.Model):
+    name = models.CharField(max_length=50)
+    image = FileField()
+
+class ImageType(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    width = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+    imagedoc = models.ForeignKey('ImageDocument', on_delete=CASCADE)

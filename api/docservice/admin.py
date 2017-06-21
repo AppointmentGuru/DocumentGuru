a9611a3c9f3ea79e70420acabbcc32b98a8c5ebe
SoldierGamma <michael@tangentsolutions.co.za>
2017-06-21 09:21:22 +0200

    Re migrated database and registered models admin.py

--- api/docservice/admin.py
+++ api/docservice/admin.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
+
 from django.contrib import admin
+from .models import Document, ImageDocument, ImageType, Note
 
 # Register your models here.
+admin.site.register(Document)
+admin.site.register(ImageDocument)
+admin.site.register(ImageType)
+admin.site.register(Note)
